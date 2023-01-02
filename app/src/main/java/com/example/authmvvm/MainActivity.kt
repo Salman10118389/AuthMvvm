8ef5243b511931153fd09a7bbe489b6ca1c8af37
salman10118389 <salman.10118389@mahasiswa.unikom.ac.id>
2023-01-02 13:38:39 +0700

    Commit kedua

--- app/src/main/java/com/example/authmvvm/MainActivity.kt
+++ app/src/main/java/com/example/authmvvm/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.authmvvm
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val user: MutableMap<String, Any> = HashMap()
+
+        user["name"] = "Muhammad Salman Al-farisi"
+        user["born"] = 2000
+
+        FirebaseFirestore.getInstance().collection("users")
+            .add(user)
+            .addOnSuccessListener {
+                Log.d("TAG", "Document Snapshot added with ID: " + it.id)
+            }.addOnFailureListener{
+                Log.w("TAG", "Error Adding Document", )
+            }
     }
 }
