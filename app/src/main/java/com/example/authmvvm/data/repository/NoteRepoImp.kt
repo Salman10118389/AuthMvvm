c37afcd770b0b4e69bdfc4fb8fc385c43d3705de
salman10118389 <salman.10118389@mahasiswa.unikom.ac.id>
2023-01-06 09:51:32 +0700

    Daggar Hilt Impl

--- app/src/main/java/com/example/authmvvm/data/repository/NoteRepoImp.kt
+++ app/src/main/java/com/example/authmvvm/data/repository/NoteRepoImp.kt
@@ -2,11 +2,28 @@ package com.example.authmvvm.data.repository
 
 import com.example.authmvvm.data.model.Note
 import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
 
 class NoteRepoImp(val database: FirebaseFirestore): NoteRepository{
 
     override fun getNotes(): List<Note> {
         //Get data From Firebase
-        return arrayListOf()
+        return arrayListOf(
+            Note(
+                id = "001",
+                text = "Jogging",
+                date = Date()
+            ),
+            Note(
+                id = "002",
+                text = "PushUps",
+                date = Date()
+            ),
+            Note(
+                id = "001",
+                text = "Squat",
+                date = Date()
+            )
+        )
     }
 }
